71977d7bd1e7135ba1b7da8348bc8e807f9ea33a
nate-mclean <nate4495@gmail.com>
2019-09-23 12:03:48 -0600

    JSON text parsed, working on imageview

--- app/src/main/java/com/example/mcleannateandroidcodingchallenge/MainActivity.java
+++ app/src/main/java/com/example/mcleannateandroidcodingchallenge/MainActivity.java
@@ -3,9 +3,20 @@ package com.example.mcleannateandroidcodingchallenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import com.dezlum.codelabs.getjson.GetJson;
+import com.google.gson.JsonObject;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +32,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        titles.add("test 1 book");
-        titles.add("test 2 book");
-        titles.add("test 3 book");
+        //get JSON
+        try {
+            String jsonString = new GetJson().AsString("http://de-coding-test.s3.amazonaws.com/books.json");
 
-        authors.add("test 1 author");
-        authors.add("test 2 author");
-        authors.add("test 3 book");
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            for (int i = 0; i < 3 ; i++){ //jsonArray.length(); i++) {
+                JSONObject jsonobject = jsonArray.getJSONObject(i);
+
+                String title = jsonobject.getString("title");
+                titles.add(title);
+
+                String imgURL = jsonobject.getString("imageURL");
+
+                //Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
+
+                try {
+                    String author = jsonobject.getString("author");
+                    authors.add(author);
+                }
+                catch (JSONException e){
+                    authors.add("");
+                }
+            }
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        //titles.add("test 1 book");
+        //titles.add("test 2 book");
+        //titles.add("test 3 book");
+
+        //authors.add("test 1 author");
+        //authors.add("test 2 author");
+        //authors.add("test 3 book");
 
         images.add(R.drawable.test1);
         images.add(R.drawable.test2);
